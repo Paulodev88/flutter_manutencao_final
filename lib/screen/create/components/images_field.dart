d17af4c6836733b999db84a17da6e2ddf243aa30
paulodev88 <paulo.ricardodev88@gmail.com>
2021-02-06 20:44:11 -0300

    Startado a tela de Manutenção

---
+++ lib/screen/create/components/images_field.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ImagesField extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.grey[200],
+      height: 200,
+    );
+  }
+}
--- lib/screen/create/create_screen.dart
+++ lib/screen/create/create_screen.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:manutencao_parse/componets/drawer/custom_Drawer.dart';
+import 'components/images_field.dart';
 
 class CreateScreen extends StatelessWidget {
+  final labelStyle = TextStyle(
+    fontWeight: FontWeight.w600,
+    color: Colors.grey,
+    fontSize: 15,
+  );
+
+  final contentPadding = const EdgeInsets.fromLTRB(16, 10, 12, 10);
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -14,6 +25,79 @@ class CreateScreen extends StatelessWidget {
             ],
           ),
         ),
-        child: Scaffold());
+        child: Scaffold(
+          appBar: AppBar(
+            title: Text(
+              "Criar Manutenção",
+              style: TextStyle(color: Colors.white),
+            ),
+            centerTitle: true,
+          ),
+          drawer: CustomDrawer(),
+          body: Card(
+            shape:
+                RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+            elevation: 8,
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                ImagesField(),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Nome do equipamento",
+                    labelStyle: labelStyle,
+                    contentPadding: contentPadding,
+                  ),
+                ),
+                Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            labelText: "Tensão",
+                            labelStyle: labelStyle,
+                            contentPadding: contentPadding,
+                            suffixText: "V"),
+                        keyboardType: TextInputType.number,
+                        onChanged: (text) {},
+                      ),
+                    ),
+                    SizedBox(
+                      width: 10,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        decoration: InputDecoration(
+                            labelText: "Corrente",
+                            labelStyle: labelStyle,
+                            contentPadding: contentPadding,
+                            suffixText: "A"),
+                        keyboardType: TextInputType.number,
+                        onChanged: (text) {},
+                      ),
+                    ),
+                  ],
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "Patrimônio",
+                    labelStyle: labelStyle,
+                    contentPadding: contentPadding,
+                  ),
+                ),
+                TextFormField(
+                  decoration: InputDecoration(
+                    labelText: "TAG",
+                    labelStyle: labelStyle,
+                    contentPadding: contentPadding,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ));
   }
 }
+
+//maxlines: null
